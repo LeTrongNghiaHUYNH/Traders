50d23afa22b0998a87acb0a06eca943437d99e74
elfaus <e.elfaus@gmail.com>
2016-02-29 15:03:02 +0100

    init repository with standard packages and classes removing sample classes

---
+++ src/models/trade/AbstractTrade.java
@@ -0,0 +1,7 @@
+package models.trade;
+
+/**
+ * Created by elfaus on 29/02/2016.
+ */
+public abstract class AbstractTrade {
+}
---
+++ src/models/user/User.java
@@ -0,0 +1,7 @@
+package models.user;
+
+/**
+ * Created by elfaus on 29/02/2016.
+ */
+public class User {
+}
