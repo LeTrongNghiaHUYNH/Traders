b84e26760a98d58385ff07c127f91e753c563151
warlof <e.elfaus@gmail.com>
2016-03-02 13:19:45 +0100

    provide a way to check if a string represent a valid stock remove the stock array (use Stock enum instead)

--- src/server/Server.java
+++ src/server/Server.java
@@ -2,7 +2,9 @@ package server;
 
 import models.trade.Ask;
 import models.trade.Bid;
+import models.trade.Stock;
 import models.user.User;
+
 import server.app.AppServer;
 import server.rpc.TraderXmlRpcServer;
 
@@ -16,8 +18,6 @@ public class Server {
     public static int rpcPort = 8080;
     public static int appPort = 9495;
 
-    public static String stocks[] = {"AAPL", "IBM", "MSFT", "ORCL"};
-
     public static ArrayList<Ask> asks;
     public static ArrayList<Bid> bids;
     public static ArrayList<User> users;
@@ -27,10 +27,28 @@ public class Server {
         bids = new ArrayList();
         users = new ArrayList();
 
+        // run both cmdLine and RPC server in separate threads
         new AppServer(Server.appPort).start();
         new TraderXmlRpcServer(Server.rpcPort).start();
 
+        // infinite loop in order to avoid the server termination
         while (true);
+    }
+
+    /**
+     * Enable to check if the specified string is a valid stock
+     * @param searchedStock The stock we want check the existence
+     * @return True if the stock exists
+     */
+    public static boolean isStockExists(String searchedStock)
+    {
+        try {
+            Stock.valueOf(searchedStock);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+
+        return true;
     }
 
 }
